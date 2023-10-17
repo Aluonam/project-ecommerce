b9f9bcfba65ef50cb216e2df1185602b635f2a39
Paula Cano <paulacanoroman@gmail.com>
2023-10-17 13:53:13 +0200

    imagen

--- e-commerce/pages/PUBLIC/Components/Products/Products.jsx
+++ e-commerce/pages/PUBLIC/Components/Products/Products.jsx
@@ -10,8 +10,8 @@ const Products = () => {
           <div className={styleCSS.products_card__title}>
             <h3>Titulillo</h3>
             </div>
-          <div className={styleCSS.products_card__img}>
-              <img></img>
+          <div className={styleCSS.products_card__imgBox}>
+              <img src='https://cdn.pixabay.com/photo/2017/01/25/17/35/picture-2008484_1280.png' className={styleCSS.products_card__productImg}></img>
             </div>
           <div className={styleCSS.products_card__price}>precio</div>
 
