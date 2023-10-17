cffda0e43d05822f482fb9b073f2ab9e31c5ca81
Paula Cano <paulacanoroman@gmail.com>
2023-10-17 10:33:42 +0200

    estilos tarjetas

--- e-commerce/pages/PUBLIC/Components/Products/Products.jsx
+++ e-commerce/pages/PUBLIC/Components/Products/Products.jsx
@@ -4,7 +4,19 @@ import styleCSS from '@/pages/PUBLIC/Components/Products/Products.module.css'
 
 const Products = () => {
   return (
-    <div className={styleCSS.products}>Products</div>
+    <div className={styleCSS.products}>
+      <div className={styleCSS.products_card}>
+
+          <div className={styleCSS.products_card__title}>
+            <h3>Titulillo</h3>
+            </div>
+          <div className={styleCSS.products_card__img}>
+              <img></img>
+            </div>
+          <div className={styleCSS.products_card__price}>precio</div>
+
+      </div>
+    </div>
   )
 }
 
