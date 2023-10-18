8d0cafecfd689268a60b1cdf7f5b5ce7863269e3
Paula Cano <paulacanoroman@gmail.com>
2023-10-18 23:37:07 +0200

    estilos con muchas tarjetas de producto

--- e-commerce/pages/PUBLIC/Components/Products/Products.jsx
+++ e-commerce/pages/PUBLIC/Components/Products/Products.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React  from 'react'
 import styleCSS from '@/pages/PUBLIC/Components/Products/Products.module.css'
 
 
 const Products = () => {
+
   return (
-    <div className={styleCSS.products}>
       <div className={styleCSS.products_card}>
 
           <div className={styleCSS.products_card__title}>
-            <h3>Titulillo</h3>
+            <h3>Title</h3>
             </div>
           <div className={styleCSS.products_card__imgBox}>
               <img src='https://cdn.pixabay.com/photo/2017/01/25/17/35/picture-2008484_1280.png' className={styleCSS.products_card__productImg}></img>
@@ -16,7 +16,6 @@ const Products = () => {
           <div className={styleCSS.products_card__price}>precio</div>
 
       </div>
-    </div>
   )
 }
 
