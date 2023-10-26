f5513e93e05a0d4446d3d836c3237c67175c77ec
Paula Cano <paulacanoroman@gmail.com>
2023-10-26 21:22:16 +0200

    cabecera de filtros dinámico
    con useState, useEffect, map

--- e-commerce/pages/PUBLIC/Components/Filter/Filter.jsx
+++ e-commerce/pages/PUBLIC/Components/Filter/Filter.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styleCSS from '@/pages/PUBLIC/Components/Filter/Filter.module.css'
 import Button from '../Button/Button'
+import axios from 'axios'
 
 const Filter = ({setData}) => {
+
+  const [categoriesProducts, setCategoriesProducts] = useState([])
+
+  //useEffect para que se haga la llamada segun se cargan los filtros
+  useEffect(() => {
+    const apiUrl = 'http://localhost:3032/v1/fakestoreapi/getAllCategories'; 
+    axios.get(apiUrl)
+      .then(response => setCategoriesProducts(response.data))
+      .catch(error => console.log('Error detected:', error));
+  }, [])
+
+
+  const showCategory = categoriesProducts.map((actualElement)=>{
+    return(
+        <Button buttonName={actualElement.categoryName} categoryId={actualElement.categoryId} setData={setData}></Button>
+    )
+  })
+
+
   return (
     <div className={styleCSS.filter}>
-      <Button buttonName={"electrónica"} categoryId={2} setData={setData}></Button>
+      {showCategory}
     </div>
   )
 }
