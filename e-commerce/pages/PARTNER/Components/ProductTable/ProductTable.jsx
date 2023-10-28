c4706e56572eb985880d325efbc1c7c4404d783a
Paula Cano <paulacanoroman@gmail.com>
2023-10-28 19:30:26 +0200

    boxModel tabla estilos

--- e-commerce/pages/PARTNER/Components/ProductTable/ProductTable.jsx
+++ e-commerce/pages/PARTNER/Components/ProductTable/ProductTable.jsx
@@ -4,25 +4,31 @@ import styleProductTable from './ProductTable.module.css'
 
 const ProductTable = () => {
   return (
-    <div className={styleProductTable.GlobalProductTable}>
-        <div>ProductTable</div>
-        <table>
-            <thead>
-                <tr>
-                    <th>nombre del producto</th>
-                    <th>categoría</th>
-                    <th>precio</th>
-                </tr>
-                <tr>
-                    <td>producto 1</td>
-                    <td>categoría 1</td>
-                    <td>precio 1</td>
-                </tr>
-
+    <>
+    <div className={styleProductTable.titleOfTheTable}>ProductTable</div>
+     <div className={styleProductTable.GlobalProductTable}>
+        
+        <table className={styleProductTable.Table}>
+            <thead className={styleProductTable.theadTable}>
+                
+                    <th className={styleProductTable.columnNameProduct}>nombre del producto</th>
+                    <th className={styleProductTable.columnCategory}>categoría</th>
+                    <th className={styleProductTable.columnPrice}>precio</th>
+                
             </thead>
+            <tbody className={styleProductTable.tbodyTable}>
+                
+                    <td className={styleProductTable.columnNameProduct}>producto 1</td>
+                    <td className={styleProductTable.columnCategory}>categoría 1</td>
+                    <td className={styleProductTable.columnPrice}>precio 1</td>
+                
+            </tbody>
+            
         </table>
     
     </div>
+    </>
+   
   )
 }
 
