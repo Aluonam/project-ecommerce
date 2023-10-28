ac0b03a1b4d9af41866d9503de03dbd35f8b9833
Paula Cano <paulacanoroman@gmail.com>
2023-10-28 18:29:54 +0200

    feat: tabla básica

--- e-commerce/pages/PARTNER/Components/ProductTable/ProductTable.jsx
+++ e-commerce/pages/PARTNER/Components/ProductTable/ProductTable.jsx
@@ -4,7 +4,25 @@ import styleProductTable from './ProductTable.module.css'
 
 const ProductTable = () => {
   return (
-    <div className={styleProductTable.GlobalProductTable}>ProductTable</div>
+    <div className={styleProductTable.GlobalProductTable}>
+        <div>ProductTable</div>
+        <table>
+            <thead>
+                <tr>
+                    <th>nombre del producto</th>
+                    <th>categoría</th>
+                    <th>precio</th>
+                </tr>
+                <tr>
+                    <td>producto 1</td>
+                    <td>categoría 1</td>
+                    <td>precio 1</td>
+                </tr>
+
+            </thead>
+        </table>
+    
+    </div>
   )
 }
 
