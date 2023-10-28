e162514509dabd74b55f2550216e1a401af0b1f8
Paula Cano <paulacanoroman@gmail.com>
2023-10-28 18:19:10 +0200

    boxModel

---
+++ e-commerce/pages/PARTNER/Components/ProductTable/ProductTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styleProductTable from './ProductTable.module.css'
+
+
+const ProductTable = () => {
+  return (
+    <div className={styleProductTable.GlobalProductTable}>ProductTable</div>
+  )
+}
+
+export default ProductTable
