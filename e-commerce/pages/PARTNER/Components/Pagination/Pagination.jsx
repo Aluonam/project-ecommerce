35be51b53ebae169b871b471f0f717c0972eaa11
Paula Cano <paulacanoroman@gmail.com>
2023-10-29 16:15:16 +0100

    Programación defensiva para evitar mostrar botón pag previa
    {/* programación defensiva no mostrará el botón si recibe false al comprobar prevPage */}
         {paginationData.links.prevPage && <button className={stylePagination.buttonPagination} onClick={()=>handlePage(paginationData.links.prevPage)}>pag ant</button>}

--- e-commerce/pages/PARTNER/Components/Pagination/Pagination.jsx
+++ e-commerce/pages/PARTNER/Components/Pagination/Pagination.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import stylePagination from './Pagination.module.css'
 
 const Pagination = ({paginationData}) => {
 
   console.log(paginationData, "paginacionnnnn")
-
+  
+  const handlePage = (endURL)=>{
+    const apiUrl = `http://localhost:3032/v1${endURL}`
+    axios.get(apiUrl)
+    .then(response => {
+                      })
+  }
 
   return (
     <div className={stylePagination.prueba}>
-      <button className={stylePagination.buttonPagination}>pag ant</button>
+      {/* programación defensiva no mostrará el botón si recibe false al comprobar prevPage */}
+     {paginationData.links.prevPage && <button className={stylePagination.buttonPagination} onClick={()=>handlePage(paginationData.links.prevPage)}>pag ant</button>} 
       <div> {paginationData.pageData.actualPage} de {paginationData.pageData.totalPages} </div>
       <button className={stylePagination.buttonPagination}>pag sig</button>
     </div>
