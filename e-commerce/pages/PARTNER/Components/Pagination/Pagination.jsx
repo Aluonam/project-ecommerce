776d317d404bd7739fbf51768a3337147520d84b
Paula Cano <paulacanoroman@gmail.com>
2023-10-29 16:17:32 +0100

    conditional chain (poner ? por si no llega ahi el objeto)
     {/* programación defensiva no mostrará el botón si recibe false al comprobar prevPage */}
         {paginationData.links?.prevPage && <button className={stylePagination.buttonPagination} onClick={()=>handlePage(paginationData.links.prevPage)}>pag ant</button>}
          <div> {paginationData.pageData?.actualPage} de {paginationData.pageData?.totalPages} </div>

--- e-commerce/pages/PARTNER/Components/Pagination/Pagination.jsx
+++ e-commerce/pages/PARTNER/Components/Pagination/Pagination.jsx
@@ -15,8 +15,8 @@ const Pagination = ({paginationData}) => {
   return (
     <div className={stylePagination.prueba}>
       {/* programación defensiva no mostrará el botón si recibe false al comprobar prevPage */}
-     {paginationData.links.prevPage && <button className={stylePagination.buttonPagination} onClick={()=>handlePage(paginationData.links.prevPage)}>pag ant</button>} 
-      <div> {paginationData.pageData.actualPage} de {paginationData.pageData.totalPages} </div>
+     {paginationData.links?.prevPage && <button className={stylePagination.buttonPagination} onClick={()=>handlePage(paginationData.links.prevPage)}>pag ant</button>} 
+      <div> {paginationData.pageData?.actualPage} de {paginationData.pageData?.totalPages} </div>
       <button className={stylePagination.buttonPagination}>pag sig</button>
     </div>
   )
